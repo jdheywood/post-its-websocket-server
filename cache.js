bdf1f27823da117f5cd304471829025d1013df08
James Heywood <jdheywood@gmail.com>
2016-11-21 22:39:56 +0000

    Comments on caching and whether to replace or enhance client syncing

--- cache.js
+++ cache.js
@@ -13,7 +13,9 @@ module.exports = {
   message(message) {
     // TODO determine type of message, and cache into the relevant collection
 
-    // TODO if message of type user, broadcast other users and notes - once working remove this from the clients?
+    // TODO if message of type user, broadcast other users and notes -
+    // once working remove this from the clients? or do we want both cnetral and distributed?
+    // if the clients can treat messages as idempotent it doesn;t matter right?
 
     // TODO if message of type note, do nothing further as it will echo that out anyway
 
